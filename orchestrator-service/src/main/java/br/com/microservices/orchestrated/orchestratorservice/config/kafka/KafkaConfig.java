a95c3de381b8d520cb60a3af6bbd94deaeea0545
felipe.franca <felipe.franca@sovos.com>
2024-09-18 17:39:54 -0300

    Incluindo anotação faltante nas Configs do KafkaConfig

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/kafka/KafkaConfig.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/kafka/KafkaConfig.java
@@ -9,11 +9,12 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@EnableKafka
 @Configuration
 @RequiredArgsConstructor
 public class KafkaConfig {
