1e6db1c22511998b934cb2b542b455cceca27cd5
felipe.franca <felipe.franca@sovos.com>
2024-10-02 19:54:57 -0300

    Ajustando anotação para payment-service

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Product.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Product.java
@@ -3,12 +3,11 @@ package br.com.microservices.orchestrated.paymentservice.core.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Document
+
 public class Product {
 
     private String code;
