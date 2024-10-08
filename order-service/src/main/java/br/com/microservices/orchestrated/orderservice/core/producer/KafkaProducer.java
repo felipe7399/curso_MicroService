a64d97804a11e09257a9b395a815db0ae944d04a
felipe.franca <felipe.franca@sovos.com>
2024-10-07 21:34:40 -0300

    criando Consumers

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/producer/KafkaProducer.java
@@ -0,0 +1,31 @@
+package br.com.microservices.orchestrated.orderservice.core.producer;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaProducer {
+
+private KafkaTemplate<String,String> kafkaTemplate;
+
+@Value("${spring.kafka.topic.start-saga}")
+private String topic;
+
+public void sendEvent (String payload) {
+
+    try{
+        log.info("Sending Event to topic {} with data {}", topic , payload);
+        kafkaTemplate.send(topic, payload);
+    }catch (Exception e){
+      log.error("Error sending topic {} containing data {}", topic,payload);
+    }
+
+}
+
+}
