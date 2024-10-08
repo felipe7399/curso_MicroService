c33f9ee0d3d151cd5b1c068ee1bacbd8a97e48e8
felipe.franca <felipe.franca@sovos.com>
2024-10-07 23:28:05 -0300

    Ajustando erros de anotações

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Product.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Product.java
@@ -3,12 +3,10 @@ package br.com.microservices.orchestrated.orchestratorservice.core.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Document
 public class Product {
 
     private String code;
