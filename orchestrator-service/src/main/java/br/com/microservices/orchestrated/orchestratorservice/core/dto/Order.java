370d069b3f75333860f5143cc4526b0b62fc2689
felipe.franca <felipe.franca@sovos.com>
2024-10-07 23:27:59 -0300

    Ajustando erros de anotações

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Order.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Order.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,7 +15,6 @@ import java.util.List;
 @Builder
 public class Order {
 
-    @Id
     private String id;
     private List<OrderProducts> products;
     private double totalAmount;
