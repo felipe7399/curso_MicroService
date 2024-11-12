569f319a15d6822dc0e2697672f7390b3fbae843
felipe.franca <felipe.franca@sovos.com>
2024-11-11 23:05:31 -0300

    Product-validation-service - Criando arquivo para inserção inicial de dados

--- product-validation-service/src/main/resources/inseert.sql
+++
@@ -1,4 +0,0 @@
-insert into public.product(id,code) values (1, 'COMIC_BOOK')
-insert into public.product(id,code) values (2, 'BOOK')
-insert into public.product(id,code) values (3, 'MOVIES')
-insert into public.product(id,code) values (4, 'MUSIC')
